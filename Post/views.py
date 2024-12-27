a33b15cc5b4e8a6f3973ebe02699b8c702a3879e
Md Sujan Mia <mdsujanmia860@gmail.com>
2024-12-28 00:24:08 +0600

    add Edit Function

--- Post/views.py
+++ Post/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect 
-from . import forms 
+from . import forms
+from . import models 
 
 
 def add_post(request):
@@ -12,3 +13,18 @@ def add_post(request):
         post_form = forms.PostForm()
     return render(request, 'add_post.html', {'form' : post_form})
 
+def edit_post(request, id):
+    
+    post = models.Post.objects.get(pk=id)
+    post_form = forms.PostForm(instance=post)
+    
+    if request.method == 'POST':
+        post_form = forms.PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('homepage')
+        
+    # else:
+    #     post_form = forms.PostForm()
+    return render(request, 'add_post.html', {"form" : post_form})  
+  
