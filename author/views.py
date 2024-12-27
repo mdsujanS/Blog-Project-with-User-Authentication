578c22b72ac979857306cbfc40290bf7c53bb92a
Md Sujan Mia <mdsujanmia860@gmail.com>
2024-12-27 20:58:06 +0600

    Create Author model, forms and install crispy form

--- author/views.py
+++ author/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from . import forms 
 
-# Create your views here.
 
 def add_author(request):
-    pass
+    
+    author_form = forms.AuthorForm()
+    return render(request, 'add_author.html', {'form' : author_form})
+
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +44,10 @@ INSTALLED_APPS = [
     'Profile',
     'Post',
 ]
+# CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,7 +64,7 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
