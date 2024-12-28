fbf9a6beb42c514152182ae199b19ec1cba26b8b
Md Sujan Mia <mdsujanmia860@gmail.com>
2024-12-29 00:50:51 +0600

    comment all model registation

--- author/admin.py
+++ author/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from .models import Author
+# from django.contrib import admin
+# from .models import Author
 
-admin.site.register(Author)
+# admin.site.register(Author)
 
