d577f2ce3dabff70c25873f138aa7769e8930fd6
Md Sujan Mia <mdsujanmia860@gmail.com>
2024-12-28 12:16:26 +0600

    Add Django Builtin Registation form

--- author/views.py
+++ author/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, redirect
 from . import forms 
 
+from django.contrib import messages 
+
+# def add_author(request):
+#     if request.method == 'POST':
+#         author_form = forms.AuthorForm(request.POST)
+#         if author_form.is_valid():
+#             author_form.save()
+#             return redirect('add_author')
+#     else:
+#         author_form = forms.AuthorForm()
+        
+#     return render(request, 'add_author.html', {'form' : author_form})
 
 def add_author(request):
     if request.method == 'POST':
-        author_form = forms.AuthorForm(request.POST)
-        if author_form.is_valid():
-            author_form.save()
+        register_form = forms.RegistrationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, 'Account Create Successfully')
             return redirect('add_author')
     else:
-        author_form = forms.AuthorForm()
-        
-    return render(request, 'add_author.html', {'form' : author_form})
-
+        register_form = forms.RegistrationForm()
+    return render(request, 'add_author.html', {'form' : register_form, 'type' : 'Register'})
