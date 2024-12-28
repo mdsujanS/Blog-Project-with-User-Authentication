5c8417b09009631e5959c333c97adc081748ae6f
Md Sujan Mia <mdsujanmia860@gmail.com>
2024-12-29 00:49:12 +0600

    Comment models from

--- author/models.py
+++ author/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-    bio = models.TextField()
-    phone_number = models.CharField(max_length=15)
+# class Author(models.Model):
+#     name = models.CharField(max_length=100)
+#     bio = models.TextField()
+#     phone_number = models.CharField(max_length=15)
     
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
