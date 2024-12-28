84ea8164e7db6ee8e916c9e178d98db3f11ddb8c
Md Sujan Mia <mdsujanmia860@gmail.com>
2024-12-28 13:41:20 +0600

    Add user_login function

--- author/views.py
+++ author/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from . import forms 
 
 from django.contrib import messages 
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.forms import AuthenticationForm
 
 # def add_author(request):
 #     if request.method == 'POST':
@@ -20,7 +22,27 @@ def add_author(request):
         if register_form.is_valid():
             register_form.save()
             messages.success(request, 'Account Create Successfully')
-            return redirect('add_author')
+            return redirect('login')
     else:
         register_form = forms.RegistrationForm()
     return render(request, 'add_author.html', {'form' : register_form, 'type' : 'Register'})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['username']
+            user_pass = form.cleaned_data['password']
+            user = authenticate(username=user_name, password= user_pass)
+            
+            if user is not None:
+                messages.success(request, 'Logged in Sucessfully')
+                login(request, user)
+                return redirect('homepage')
+            else:
+                messages.warning(request, "Login information incorrect")
+                return redirect('add_author')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form' : form })
