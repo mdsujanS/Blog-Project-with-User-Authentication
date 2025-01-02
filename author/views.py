7def8e59a1cf6829f5d956302088579787a155b0
Md Sujan Mia <mdsujanmia860@gmail.com>
2025-01-02 13:55:27 +0600

    remove userdefine add athor function

--- author/views.py
+++ author/views.py
@@ -6,16 +6,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages 
 
-# def add_author(request):
-#     if request.method == 'POST':
-#         author_form = forms.AuthorForm(request.POST)
-#         if author_form.is_valid():
-#             author_form.save()
-#             return redirect('add_author')
-#     else:
-#         author_form = forms.AuthorForm()
-        
-#     return render(request, 'add_author.html', {'form' : author_form})
 
 def register(request):
     if request.method == 'POST':
