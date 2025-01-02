da439f5f8c920a7b01e8bfea7835b63e40e71f93
Md Sujan Mia <mdsujanmia860@gmail.com>
2025-01-02 14:13:56 +0600

    remove author models

--- Profile/models.py
+++ Profile/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-from author.models import Author 
 
+from django.contrib.auth.models import User 
+ 
 # Create your models here.
 
 class Profile(models.Model):
     name = models.CharField(max_length=100)
     about = models.TextField()
-    author = models.OneToOneField(Author, on_delete=models.CASCADE, default=None)
+    author = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
     
     def __str__(self):
         return self.name
     
+     
     
